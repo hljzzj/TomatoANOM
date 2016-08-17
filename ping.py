66c59649e288c5bf202c6226fbe56e9dc21328e8
zzj <hljzzj@qq.com>
2016-08-17 12:32:09 +0800

    OFFICE

--- ping.py
+++ ping.py
@@ -11,9 +11,8 @@ def Success(ip):
         #print "通：%s" % ip["hostIP"]
         conn = mdb.connect('127.0.0.1', 'dgcmdb', 'Dzga@110', 'dgcmdb');
         cur = conn.cursor(mdb.cursors.DictCursor)
-        cur.execute(
-            "INSERT INTO website_serverhostrecord(hostIP_id, status_id,updateTime ) VALUES ('%s',1,now())" % ip["id"])
-        cur.execute("UPDATE website_ServerHostList SET status_id = 1,updatetime = now() WHERE id = %s" % ip["id"])
+        cur.execute("INSERT INTO website_serverhostrecord(hostIP_id, status_id,updateTime ) VALUES ('%s',1,now())" % ip["id"])
+        cur.execute("UPDATE website_serverhost SET status_id = 1,updatetime = now() WHERE id = %s" % ip["id"])
         cur.close()
     except:
         print '%s\t 运行失败,失败原因' % ip["hostIP"]
@@ -25,25 +24,14 @@ def Fail(ip):
         #print "不通：%s" % ip["hostIP"]
         conn = mdb.connect('127.0.0.1', 'dgcmdb', 'Dzga@110', 'dgcmdb');
         cur = conn.cursor(mdb.cursors.DictCursor)
-        cur.execute(
-            "INSERT INTO website_serverhostrecord(hostIP_id, status_id,updateTime ) VALUES ('%s',2,now())" % ip["id"])
-
-
-        cur.execute("UPDATE website_ServerHostList SET status_id = 2 WHERE id = %s" % ip["id"])
-
+        cur.execute("INSERT INTO website_serverhostrecord(hostIP_id, status_id,updateTime ) VALUES ('%s',2,now())" % ip["id"])
+        cur.execute("UPDATE website_serverhost SET status_id = 2 WHERE id = %s" % ip["id"])
     except :
         print '%s\t 运行失败,失败原因' % ip["hostIP"]
         info = sys.exc_info()
         print info[0], ":", info[1]
-
-
-
-
 def check(i, q):
     i = q.get()  # 获取Queue队列传过来的ip，队列使用队列实例queue.put(ip)传入ip，通过q.get() 获得
-    # print "Thread %s:Pinging %s,创建线程成功,开始扫描" % (i, ip["hostIP"])
-    # print "i:%s" % i
-    # print "q:%s" % q
     try:
         data = os.system("ping -c 1 %s>/dev/null 2>&1" % i["hostIP"])
     except:
@@ -56,20 +44,15 @@ def check(i, q):
     else:
         Fail(i)
 
-
-
-
-
-
 if __name__=='__main__':
     while True:
         queue = Queue.Queue()
         try:
             conn = mdb.connect('127.0.0.1', 'dgcmdb', 'Dzga@110', 'dgcmdb');
             cur = conn.cursor(mdb.cursors.DictCursor)
-            cur.execute("SELECT hostIP,id,status_id FROM website_ServerHostList")
+            cur.execute("SELECT hostIP,id,status_id FROM website_serverhost")
             rows = cur.fetchall()
-
+            print rows
             cur.close()
         except:
             print "连接出错"
@@ -86,6 +69,5 @@ if __name__=='__main__':
         for th in threads:
             th.join()
         print "程序结束运行%s" % datetime.datetime.now()
-        threading._sleep(30)
 
 
